c9ef6e6bd7bc9e38e614e20b83f5f0d83f07bc22
tp350 <fp7623@pleasantonusd.net>
2025-04-22 21:49:55 +0000

    m

--- Main.java
+++ Main.java
@@ -1,22 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    //Instrument instrument = new Instrument(); //DOES THIS WORK? -> NO
-
-    //Violin violin = new Violin();
-
-    //Glockenspiel glockenspiel = new Glockenspiel();
-
-    //instrument.tune();
-    //violin.tune();
-    //glockenspiel.tune();
-
-    //instrument.play();
-    //violin.play();
-    //glockenspiel.play();
-
-    //instrument.done();
-    violin.done();
-    //glockenspiel.done();
+    new Violin().done();
+    new Glockenspiel().done();
   }
 
 }
